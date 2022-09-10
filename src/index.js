2f219c4a7cce7e0732c62619edb3d912e40489cb
Pucheo-Gideon <okwongudogideon123@gmail.com>
2022-09-09 18:42:20 -0700

    multi-card commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import Motivation from './assign1/Motivation'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        {/* <App></App> */}
-        <Motivation></Motivation>
+        <App></App>
+        {/* <Motivation></Motivation> */}
     </React.StrictMode>
   
 
